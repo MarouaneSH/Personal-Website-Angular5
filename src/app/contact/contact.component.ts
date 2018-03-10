eecfc9550770c7248d5c850371376f44f94591da
Marouane Souah <marouanesouah@gmail.com>
2018-03-09 21:43:00 -0800

    Add  SASS media query

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { routeInAnimation } from './../shared/animations';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
+  animations: [
+    routeInAnimation
+  ]
 })
 export class ContactComponent implements OnInit {
+
+  @HostBinding('@routeInAnimation') routeAnimation = true;
 
   constructor() { }
 
